084bf55375082a9f8f3df2144703516ab2f802ad
Thomas Jacquey <thomas@webcd.fr>
2015-08-11 11:52:53 +0200

    V1.0.0

--- noAds.js
+++ noAds.js
@@ -4,8 +4,7 @@
  */
 var PROXY_DIRECT = "DIRECT";
 var DIRECT = "DIRECT";
-//var BLACK = "PROXY 8.8.8.8:53";
-var BLACK = "PROXY 0.0.0.0:3421";
+var BLACK = "PROXY 0.0.0.0:8080";
 var WHITE = PROXY_DIRECT;
 
 /**
